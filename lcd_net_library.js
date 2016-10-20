139e71bd29e185e03344ababe2cf41f840806695
kodorobot <kodorobot@gmail.com>
2016-10-20 17:31:09 +0800

    Add files via upload
    change name

---
+++ lcd_net_library.js
@@ -0,0 +1,339 @@
+/*
+ *This program is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+(function(ext) {
+	
+    var isConnected = false;
+	var sensor_data = {};
+    var poll_request = setInterval(send_poll, 30);
+
+    ext._getStatus = function () {
+        if (isConnected) return { status: 2, msg: 'Okay' };
+        if (!isConnected) return { status: 1, msg: 'no product is running' };
+    };
+	
+    ext._shutdown = function() {
+        if (poll_request) {
+          clearInterval(poll_request);
+          poll_request = null;
+        }
+  };
+  
+    ext.connect = function () {
+
+    }
+	
+    ext.digital_pin_mode = function (able, pin, mode) {
+        if(able == "啟用") able = "%E5%95%9F%E7%94%A8";
+        else if(able == "停用") able = "%E5%81%9C%E7%94%A8";
+		
+        if(mode == "輸入") mode = "%E8%BC%B8%E5%85%A5";
+        else if(mode == "輸出") mode = "%E8%BC%B8%E5%87%BA";
+        else if(mode == "伺服機") mode = "%E4%BC%BA%E6%9C%8D%E6%A9%9F";
+        else if(mode == "音調") mode = "%E9%9F%B3%E8%AA%BF";
+        else if(mode == "輸入(pull-up)") mode = "pull-up";
+        else if(mode == "輸入(pull-down)") mode = "pull-down";
+
+        send("/digital_pin_mode/" + able + "/" + pin + "/" + mode);
+	}
+	
+    ext.analog_pin_mode = function (able, pin, mode){
+        if(able == "啟用") able = "%E5%95%9F%E7%94%A8";
+        else if(able == "停用") able = "%E5%81%9C%E7%94%A8";
+        
+        if(mode == "輸入") mode = "%E8%BC%B8%E5%85%A5";
+        else if(mode == "輸入(pull-up)") mode = "pull-up";
+        else if(mode == "輸入(pull-down)") mode = "pull-down";
+		
+        send("/analog_pin_mode/" + able + "/" + pin + "/" + mode);
+		
+	}
+	
+    ext.digital_write = function(pin, value){
+        send("/digital_write/" + pin + "/" + value);
+	}
+	
+    ext.analog_write = function(pin,value){
+        send("/analog_write/" + pin + "/" + value);
+	}
+	
+    ext.play_tone = function(pin, frequency, time){
+        send("/play_tone/" + pin + "/" + frequency + "/" + time);
+	}
+	
+    ext.tone_off = function(pin){
+        send("/tone_off/" + pin);
+	}
+	
+    ext.set_servo_position = function(pin, angle){
+        send("/set_servo_position/" + pin + "/" + angle);
+	}
+	
+    ext.digital_read = function(pin){
+        var pin = "digital_read/" + pin;
+        var value = sensor_data[pin];
+        return value;
+	}
+	
+    ext.analog_read = function(pin){
+        var pin = "analog_read/" + pin;
+        var value = sensor_data[pin];
+        return value;
+	}
+    
+    ext.lcd_initial = function(){
+        send("/lcd_initial/");
+	}
+    
+    ext.lcd_print_cover = function(value, num1, num2){
+        send("/lcd_print_cover/" + "/" + value + "/" + num1 + "/" + num2);
+	}
+    
+    ext.back_light_on = function(){
+        send("/back_light_on/");
+	}
+    
+    ext.back_light_off = function(){
+        send("/back_light_off/");
+	}
+    
+    ext.lcd_clear = function(){
+        send("/lcd_clear/");
+	}
+    
+    ext.lcd_scrollDisplayLeft = function(){
+        send("/lcd_scrollDisplayLeft/");
+	}
+    
+    ext.lcd_scrollDisplayRight = function(){
+        send("/lcd_scrollDisplayRight/");
+	}
+    
+    ext.s0 = function(){
+        return sensor_data["s0"];
+	}
+	
+    ext.s1 = function(){
+        return sensor_data["s1"];
+	}
+	
+    ext.sensor_update_scratch = function(ip, key, value){
+        send("/sensor_update_scratch/" + "/" + ip + "/" + key + "/" + value);
+	}
+	
+    ext.sensor_update = function(key, value){
+        send("/sensor_update/" + "/" + key + "/" + value);
+	}
+	
+    ext.HTTPvalue = function(){
+        return sensor_data["HTTPvalue"];
+	}
+    
+    ext.HTTPvalue_number = function(){
+        return sensor_data["HTTPvalue_number"];
+	}
+    
+    ext.HTTPvalue_processed = function(){
+        return sensor_data["HTTPvalue_processed"];
+	}
+    
+    ext.HTTP_allkeyValue = function(){
+        return sensor_data["HTTP_allkeyValue"];
+	}
+    
+    ext.HTTP_keyValue = function(){
+        return sensor_data["HTTP_keyValue"];
+	}
+	
+    ext.httpPOST = function(url){
+        url = replaceAll(url,"/","%2F")
+        url = replaceAll(url,"&","%26")
+        url = replaceAll(url,"?","%3F")
+        send("/httpPOST/" + url);
+	}
+	
+    ext.httpGET_type = function(type, url){
+        url = replaceAll(url,"/","%2F")
+        url = replaceAll(url,"&","%26")
+        url = replaceAll(url,"?","%3F")
+        send("/httpGET_type/" + type + "/" + url);
+	}
+    
+    ext.httpGET_database = function(type, value){
+        send("/httpGET_database/" + type + "/" + value);
+	}
+    
+    ext.jsonDataSelect_thingspeak = function(type, value){
+        send("/jsonDataSelect_thingspeak/" + type + "/" + value);
+	}
+    
+    ext.jsonDataSelect = function(type, value){
+        send("/jsonDataSelect/" + type + "/" + value);
+	}
+	
+    ext.keyFind = function(){
+        send("/keyFind/");
+	}
+    
+    ext.keySelect = function(url){
+        send("/keySelect/" + url);
+	}
+    
+    ext.datavalue = function(){
+        return sensor_data["datavalue"];
+    }
+    
+    ext.opendata = function(value){
+        send("/opendata/" + value);
+    }
+    
+    ext.opendata2 = function(value, num){
+        send("/opendata2/" + value + "/" + num);
+    }
+    
+    ext.writedata = function(input, value){
+        send("/writedata/" + input + "/" + value);
+    }
+    
+    ext.appenddata = function(input, value){
+        send("/appenddata/" + input + "/" + value);
+    }
+    
+    ext.open_notepad = function(value){
+        send("/open_notepad/" + value);
+    }
+    
+    ext.openBrowser = function(url){
+        url = replaceAll(url,"/","%2F")
+        url = replaceAll(url,"&","%26")
+        url = replaceAll(url,"?","%3F")
+        send("/openBrowser/" + url);
+    }
+    
+    ext.humidity_dht11 = function(){
+        return sensor_data["humidity_dht11"];
+    }
+    
+    ext.temperature_dht11 = function(){
+        return sensor_data["temperature_dht11"];
+    }
+    
+    ext.dht11 = function(value){
+        send("/dht11/" + value);
+    }
+    
+    ext.distance_HC_SR04 = function(){
+        return sensor_data["distance_HC-SR04"];
+    }
+    
+    ext.distance = function(pin, pin2){
+        send("/distance/" + pin + "/" + pin2);
+    }
+		
+    function send(cmd) {
+        //connection.send(cmd);
+        var http = new XMLHttpRequest();
+        http.open("POST", "http://127.0.0.1:50209" + cmd, true);
+        http.onreadystatechange = function() {
+            if (http.readyState == 4) {
+                console.log(http.responseText);
+            }
+        }
+        http.send();
+    }
+	
+    function send_poll() {
+        var http = new XMLHttpRequest();
+        http.open("GET", "http://127.0.0.1:50209/poll", true);
+        http.onreadystatechange = function() {
+            if (http.readyState == 4) {
+                if (http.responseText.length != 0){
+                    if (!isConnected) isConnected = true;
+                    var sensor = http.responseText.split("\n");
+                    for(var i = 0;i < sensor.length;i++) sensor_data[sensor[i].split(" ")[0].toString()] = sensor[i].split(" ")[1];
+                }
+                else isConnected = false;
+            }
+        }
+        http.send();
+    }
+	
+    function replaceAll(str, find, replace) {
+        while(str.indexOf(find) >= 0) { str = str.replace(find, replace); }
+    return str;
+	}
+
+    var descriptor = {
+        blocks: [
+            [" ", "%m.pin_state : 數位腳位 %n 為 %m.digital_pin_mode", "digital_pin_mode", "啟用", "號碼", "輸入"],
+            [" ", "%m.pin_state : 類比腳位(A) %n 為 %m.analog_pin_mode", "analog_pin_mode", "啟用", "號碼", "輸入"],
+            ["", "數位輸出: 設定腳位 %n 為 %d.high_low", "digital_write", "號碼", 0],
+            ["", "模擬類比輸出(PWM): 設定腳位 %n 的值為  %n", "analog_write", "號碼", "數量值"],
+            ["", "在腳位 %n 播放音調, 頻率為: %n Hz, 時間為: %n ms", "play_tone", "號碼", 1000, 500],
+            ["", "關閉腳位 %n 的音調", "tone_off", "號碼"],
+            ["", "設定第 %n 腳位為伺服機輸出 轉動角度為 %n", "set_servo_position", "號碼", 90],
+            ["r", "讀取數位腳位 %n 的值", "digital_read", "號碼"],
+            ["r", "讀取類比腳位(A) %n 的值", "analog_read", "號碼"],
+            [" ", "SDA接A4,SCL接A5", "lcd_initial"],
+            [" ", "文字(英,數): %s 位置 行: %n 列: %n", "lcd_print_cover", "", 1, 1],
+            [" ", "開燈", "back_light_on"],
+            [" ", "關燈", "back_light_off"],
+            [" ", "清空文字", "lcd_clear"],
+            [" ", "左移", "lcd_scrollDisplayLeft"],
+            [" ", "右移", "lcd_scrollDisplayRight"],
+            ["r", "資料", "datavalue"],
+            [" ", "讀取本機資料 從 %s", "opendata", "temp.txt"],
+            [" ", "讀取本機資料 從 %s 第 %n 行", "opendata2", "temp.txt", 1],
+            [" ", "儲存資料 %s 在 %s", "writedata", "", "temp.txt"],
+            [" ", "附加資料 %s 在 %s", "appenddata", "", "temp.txt"],
+            [" ", "開啟檔案 %s", "open_notepad", "temp.txt"],
+            ["r", "virtual sensor s0",  "s0"],
+            ["r", "virtual sensor s1",  "s1"],
+            [" ", "send %s value %n", "sensor_update", "temp", 255],
+            [" ", "向ip: %s 傳送變數 %s 值 %s", "sensor_update_scratch", "127.0.0.1:50209", "s0", 0],
+            [" ", "開啟網頁 %s", "openBrowser", "http://www.kodorobot.com"],
+            ["r", "雲端資料", "HTTPvalue"],
+            ["r", "雲端資料筆數", "HTTPvalue_number"],
+            ["r", "剖析資料", "HTTPvalue_processed"],
+            ["r", "可使用的欄位", "HTTP_allkeyValue"],
+            ["r", "選擇的欄位", "HTTP_keyValue"],
+            [" ", "HTTP POST 資料 %s", "httpPOST", "https://api.thingspeak.com/update?api_key=<api_key>&field1=<value>"],
+            [" ", "HTTP GET 資料 類型:%m.type 從 %s", "httpGET_type", "json_thingspeak", "https://thingspeak.com/channels/<channel_ID>/feed.json"],
+            [" ", "HTTP GET 資料庫:%m.database channel ID: %s", "httpGET_database", "thingspeak", ""],
+            [" ", "thingspeak雲端資料選擇 欄位:%m.key 第 %n 筆的剖析資料", "jsonDataSelect_thingspeak", "field1", "1"],
+            [" ", "雲端資料選擇 欄位:%s 第 %n 筆的剖析資料", "jsonDataSelect", "field1", "1"],
+            [" ", "Data 剖析可使用的欄位", "keyFind"],
+            [" ", "Data 剖析 第 %n 筆的欄位", "keySelect", "1"],
+            ["r", "DHT11 濕度", "humidity_dht11"],
+            ["r", "DHT11 溫度", "temperature_dht11"],
+            [" ", "DHT11 溫濕度感測器(D) %n", "dht11", 2],
+            ["r", "HC-SR04 距離", "distance_HC_SR04"],
+            [" ", "距離感測器HC-SR04 trig %n echo(A) %n", "distance", 10, 0],
+		],
+        menus: {
+            type: ["raw", "json_thingspeak", "json_opendata"],
+            key: ["field1", "field2"],
+            database: ["thingspeak"],
+            pin_state: ['啟用', '停用'],
+            digital_pin_mode: ['輸入',"輸入(pull-up)","輸入(pull-down)", '輸出', 'PWM', '伺服機', '音調'],
+            analog_pin_mode: ["輸入", "輸入(pull-up)", "輸入(pull-down)"],
+            high_low: ["0", "1"],
+    },
+        url: 'https://kodorobot.github.io/scratchx/'
+  };
+
+    ScratchExtensions.register('LCD1602_net_library', descriptor, ext);
+
+
+})({});
---
+++ led_matrix_net.js
@@ -0,0 +1,346 @@
+/*
+ *This program is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+(function(ext) {
+	
+    var isConnected = false;
+	var sensor_data = {};
+    var poll_request = setInterval(send_poll, 30);
+
+    ext._getStatus = function () {
+        if (isConnected) return { status: 2, msg: 'Okay' };
+        if (!isConnected) return { status: 1, msg: 'no product is running' };
+    };
+	
+    ext._shutdown = function() {
+        if (poll_request) {
+          clearInterval(poll_request);
+          poll_request = null;
+        }
+  };
+  
+    ext.connect = function () {
+
+    }
+    
+    ext.digital_pin_mode = function (able, pin, mode) {
+        if(able == "啟用") able = "%E5%95%9F%E7%94%A8";
+        else if(able == "停用") able = "%E5%81%9C%E7%94%A8";
+		
+        if(mode == "輸入") mode = "%E8%BC%B8%E5%85%A5";
+        else if(mode == "輸出") mode = "%E8%BC%B8%E5%87%BA";
+        else if(mode == "伺服機") mode = "%E4%BC%BA%E6%9C%8D%E6%A9%9F";
+        else if(mode == "音調") mode = "%E9%9F%B3%E8%AA%BF";
+        else if(mode == "輸入(pull-up)") mode = "pull-up";
+        else if(mode == "輸入(pull-down)") mode = "pull-down";
+
+        send("/digital_pin_mode/" + able + "/" + pin + "/" + mode);
+	}
+	
+    ext.analog_pin_mode = function (able, pin, mode){
+        if(able == "啟用") able = "%E5%95%9F%E7%94%A8";
+        else if(able == "停用") able = "%E5%81%9C%E7%94%A8";
+        
+        if(mode == "輸入") mode = "%E8%BC%B8%E5%85%A5";
+        else if(mode == "輸入(pull-up)") mode = "pull-up";
+        else if(mode == "輸入(pull-down)") mode = "pull-down";
+		
+        send("/analog_pin_mode/" + able + "/" + pin + "/" + mode);
+		
+	}
+	
+    ext.digital_write = function(pin, value){
+        send("/digital_write/" + pin + "/" + value);
+	}
+	
+    ext.analog_write = function(pin,value){
+        send("/analog_write/" + pin + "/" + value);
+	}
+	
+    ext.play_tone = function(pin, frequency, time){
+        send("/play_tone/" + pin + "/" + frequency + "/" + time);
+	}
+	
+    ext.tone_off = function(pin){
+        send("/tone_off/" + pin);
+	}
+	
+    ext.set_servo_position = function(pin, angle){
+        send("/set_servo_position/" + pin + "/" + angle);
+	}
+	
+    ext.digital_read = function(pin){
+        var pin = "digital_read/" + pin;
+        var value = sensor_data[pin];
+        return value;
+	}
+	
+    ext.analog_read = function(pin){
+        var pin = "analog_read/" + pin;
+        var value = sensor_data[pin];
+        return value;
+	}
+	
+	ext.matrix_hex_result = function(){
+        return sensor_data["matrix_hex_result"];
+	}
+    
+    ext.matrix_initial = function(pin1, pin2, pin3){
+        send("/matrix_initial/" + pin1 + "/" + pin2 + "/" + pin3);
+	}
+    
+    ext.matrix_print = function(value){
+        send("/matrix_print/" + value);
+	}
+    
+    ext.matrix_print_scroll = function(value, delay){
+        send("/matrix_print_scroll/" + value + "/" + delay);
+	}
+		
+    ext.matrix_print_hex = function(value){
+        send("/matrix_print_hex/" + value);
+	}
+        
+    ext.matrix_clear = function(){
+        send("/matrix_clear/");
+	}
+    
+    ext.matrix_scrollDisplayLeft = function(){
+        send("/matrix_scrollDisplayLeft/");
+	}
+    
+    ext.matrix_scrollDisplayRight = function(){
+        send("/matrix_scrollDisplayRight/");
+	}
+    
+    ext.matrix_scrollDisplayUp = function(){
+        send("/matrix_scrollDisplayUp/");
+	}
+    
+    ext.matrix_scrollDisplayDown = function(){
+        send("/matrix_scrollDisplayDown/");
+	}
+    
+    ext.matrix_print_single = function(){
+        send("/matrix_print_single/");
+	}
+    
+    ext.show_matrix_print_single = function(value){
+        send("/show_matrix_print_single/" + value);
+	}
+    
+    ext.matrix_Intensity = function(value){
+        send("/matrix_Intensity/" + value);
+	}
+    
+    ext.s0 = function(){
+        return sensor_data["s0"];
+	}
+	
+    ext.s1 = function(){
+        return sensor_data["s1"];
+	}
+	
+    ext.sensor_update_scratch = function(ip, key, value){
+        send("/sensor_update_scratch/" + "/" + ip + "/" + key + "/" + value);
+	}
+	
+    ext.sensor_update = function(key, value){
+        send("/sensor_update/" + "/" + key + "/" + value);
+	}
+	
+    ext.HTTPvalue = function(){
+        return sensor_data["HTTPvalue"];
+	}
+    
+    ext.HTTPvalue_number = function(){
+        return sensor_data["HTTPvalue_number"];
+	}
+    
+    ext.HTTPvalue_processed = function(){
+        return sensor_data["HTTPvalue_processed"];
+	}
+    
+    ext.HTTP_allkeyValue = function(){
+        return sensor_data["HTTP_allkeyValue"];
+	}
+    
+    ext.HTTP_keyValue = function(){
+        return sensor_data["HTTP_keyValue"];
+	}
+	
+    ext.httpPOST = function(url){
+        url = replaceAll(url,"/","%2F")
+        url = replaceAll(url,"&","%26")
+        url = replaceAll(url,"?","%3F")
+        send("/httpPOST/" + url);
+	}
+	
+    ext.httpGET_type = function(type, url){
+        url = replaceAll(url,"/","%2F")
+        url = replaceAll(url,"&","%26")
+        url = replaceAll(url,"?","%3F")
+        send("/httpGET_type/" + type + "/" + url);
+	}
+    
+    ext.httpGET_database = function(type, value){
+        send("/httpGET_database/" + type + "/" + value);
+	}
+    
+    ext.jsonDataSelect_thingspeak = function(type, value){
+        send("/jsonDataSelect_thingspeak/" + type + "/" + value);
+	}
+    
+    ext.jsonDataSelect = function(type, value){
+        send("/jsonDataSelect/" + type + "/" + value);
+	}
+	
+    ext.keyFind = function(){
+        send("/keyFind/");
+	}
+    
+    ext.keySelect = function(url){
+        send("/keySelect/" + url);
+	}
+    
+    ext.datavalue = function(){
+        return sensor_data["datavalue"];
+    }
+    
+    ext.opendata = function(value){
+        send("/opendata/" + value);
+    }
+    
+    ext.opendata2 = function(value, num){
+        send("/opendata2/" + value + "/" + num);
+    }
+    
+    ext.writedata = function(input, value){
+        send("/writedata/" + input + "/" + value);
+    }
+    
+    ext.appenddata = function(input, value){
+        send("/appenddata/" + input + "/" + value);
+    }
+    
+    ext.open_notepad = function(value){
+        send("/open_notepad/" + value);
+    }
+    
+    ext.openBrowser = function(url){
+        url = replaceAll(url,"/","%2F")
+        url = replaceAll(url,"&","%26")
+        url = replaceAll(url,"?","%3F")
+        send("/openBrowser/" + url);
+    }
+        
+    function send(cmd) {
+        //connection.send(cmd);
+        var http = new XMLHttpRequest();
+        http.open("POST", "http://127.0.0.1:50209" + cmd, true);
+        http.onreadystatechange = function() {
+            if (http.readyState == 4) {
+                console.log(http.responseText);
+            }
+        }
+        http.send();
+    }
+	
+    function send_poll() {
+        var http = new XMLHttpRequest();
+        http.open("GET", "http://127.0.0.1:50209/poll", true);
+        http.onreadystatechange = function() {
+            if (http.readyState == 4) {
+                if (http.responseText.length != 0){
+                    if (!isConnected) isConnected = true;
+                    var sensor = http.responseText.split("\n");
+                    for(var i = 0;i < sensor.length;i++) sensor_data[sensor[i].split(" ")[0].toString()] = sensor[i].split(" ")[1];
+                }
+                else isConnected = false;
+            }
+        }
+        http.send();
+    }
+	
+    function replaceAll(str, find, replace) {
+        while(str.indexOf(find) >= 0) { str = str.replace(find, replace); }
+    return str;
+	}
+
+    var descriptor = {
+        blocks: [
+            [" ", "%m.pin_state : 數位腳位 %n 為 %m.digital_pin_mode", "digital_pin_mode", "啟用", "號碼", "輸入"],
+            [" ", "%m.pin_state : 類比腳位(A) %n 為 %m.analog_pin_mode", "analog_pin_mode", "啟用", "號碼", "輸入"],
+            ["", "數位輸出: 設定腳位 %n 為 %d.high_low", "digital_write", "號碼", 0],
+            ["", "模擬類比輸出(PWM): 設定腳位 %n 的值為  %n", "analog_write", "號碼", "數量值"],
+            ["", "在腳位 %n 播放音調, 頻率為: %n Hz, 時間為: %n ms", "play_tone", "號碼", 1000, 500],
+            ["", "關閉腳位 %n 的音調", "tone_off", "號碼"],
+            ["", "設定第 %n 腳位為伺服機輸出 轉動角度為 %n", "set_servo_position", "號碼", 90],
+            ["r", "讀取數位腳位 %n 的值", "digital_read", "號碼"],
+            ["r", "讀取類比腳位(A) %n 的值", "analog_read", "號碼"],
+			["r", "圖形的16位元碼", "matrix_hex_result"],
+            [" ", "Din接(D) %n ,CS接(D) %n ,CLK接(D) %n ", "matrix_initial", 10, 11, 12],
+            [" ", "顯示文字(英,數): %s", "matrix_print", ""],
+            [" ", "跑馬燈(英,數): %s 延遲: %n (ms)", "matrix_print_scroll", "", 100],
+            [" ", "顯示圖形(16位元碼) %s", "matrix_print_hex", ""],
+            [" ", "清空文字", "matrix_clear"],
+            [" ", "左移", "matrix_scrollDisplayLeft"],
+            [" ", "右移", "matrix_scrollDisplayRight"],
+            [" ", "上移", "matrix_scrollDisplayUp"],
+            [" ", "下移", "matrix_scrollDisplayDown"],
+            [" ", "自訂圖形(8x8)", "matrix_print_single"],
+            [" ", "顯示自訂圖形第 %n 張(8x8)", "show_matrix_print_single", "1"],
+            [" ", "亮度 %d.intense", "matrix_Intensity", 1],
+            ["r", "資料", "datavalue"],
+            [" ", "讀取本機資料 從 %s", "opendata", "temp.txt"],
+            [" ", "讀取本機資料 從 %s 第 %n 行", "opendata2", "temp.txt", 1],
+            [" ", "儲存資料 %s 在 %s", "writedata", "", "temp.txt"],
+            [" ", "附加資料 %s 在 %s", "appenddata", "", "temp.txt"],
+            [" ", "開啟檔案 %s", "open_notepad", "temp.txt"],
+            ["r", "virtual sensor s0",  "s0"],
+            ["r", "virtual sensor s1",  "s1"],
+            [" ", "send %s value %n", "sensor_update", "temp", 255],
+            [" ", "向ip: %s 傳送變數 %s 值 %s", "sensor_update_scratch", "127.0.0.1:50209", "s0", 0],
+            [" ", "開啟網頁 %s", "openBrowser", "http://www.kodorobot.com"],
+            ["r", "雲端資料", "HTTPvalue"],
+            ["r", "雲端資料筆數", "HTTPvalue_number"],
+            ["r", "剖析資料", "HTTPvalue_processed"],
+            ["r", "可使用的欄位", "HTTP_allkeyValue"],
+            ["r", "選擇的欄位", "HTTP_keyValue"],
+            [" ", "HTTP POST 資料 %s", "httpPOST", "https://api.thingspeak.com/update?api_key=<api_key>&field1=<value>"],
+            [" ", "HTTP GET 資料 類型:%m.type 從 %s", "httpGET_type", "json_thingspeak", "https://thingspeak.com/channels/<channel_ID>/feed.json"],
+            [" ", "HTTP GET 資料庫:%m.database channel ID: %s", "httpGET_database", "thingspeak", ""],
+            [" ", "thingspeak雲端資料選擇 欄位:%m.key 第 %n 筆的剖析資料", "jsonDataSelect_thingspeak", "field1", "1"],
+            [" ", "雲端資料選擇 欄位:%s 第 %n 筆的剖析資料", "jsonDataSelect", "field1", "1"],
+            [" ", "Data 剖析可使用的欄位", "keyFind"],
+            [" ", "Data 剖析 第 %n 筆的欄位", "keySelect", "1"],
+		],
+        menus: {
+        intense: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+        type: ["raw", "json_thingspeak", "json_opendata"],
+        key: ["field1", "field2"],
+        database: ["thingspeak"],
+        pin_state: ['啟用', '停用'],
+        digital_pin_mode: ['輸入',"輸入(pull-up)","輸入(pull-down)", '輸出', 'PWM', '伺服機', '音調'],
+        analog_pin_mode: ["輸入", "輸入(pull-up)", "輸入(pull-down)"],
+        high_low: ["0", "1"],
+
+    },
+        url: 'https://kodorobot.github.io/scratchx/'
+  };
+
+    ScratchExtensions.register('Max7219_net', descriptor, ext);
+
+
+})({});
