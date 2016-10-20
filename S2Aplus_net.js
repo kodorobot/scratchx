5120f102dfffa0f2b96aa6bb8519af431d4ded29
kodorobot <kodorobot@gmail.com>
2016-10-20 14:38:45 +0800

    Add files via upload
    test64

--- S2Aplus_net.js
+++ S2Aplus_net.js
@@ -128,9 +128,11 @@
 	}
 	
     ext.httpPOST = function(url){
-        url = replaceAll(url,"/","%2F");
-        url = replaceAll(url,"&","%26");
-        url = replaceAll(url,"?","%3F");
+        url = replaceAll(url,"/","%2F")
+        console.log(url)
+        url = replaceAll(url,"&","%26")
+        console.log(url)
+        url = replaceAll(url,"?","%3F")
         //url = replaceAll(url,"=","%3D")
         console.log(url)
         send("/httpPOST/" + url);
