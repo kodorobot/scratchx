dc449edb971677fb2dcac8a7efaa41d5cd1bfcea
kodorobot <kodorobot@gmail.com>
2016-11-25 16:46:22 +0800

    Add files via upload

--- test.js
+++ test.js
@@ -16,15 +16,7 @@
 	
     var isConnected = false;
 	var sensor_data = {};
-    
-    var poller = null;
-    
-    setTimeout(function(){ start_poll(); }, 3000);
-    
-    function start_poll(){
-        poller = setInterval(send_poll, 30);
-        console.log("ok");
-    }
+    var poll_request = setInterval(send_poll, 30);
 
     ext._getStatus = function () {
         if (isConnected) return { status: 2, msg: 'Okay' };
@@ -32,15 +24,14 @@
     };
 	
     ext._shutdown = function() {
-        if (poller) {
-          clearInterval(poller);
-          poller = null;
+        if (poll_request) {
+          clearInterval(poll_request);
+          poll_request = null;
         }
   };
   
     ext.connect = function () {
-        if (!isConnected)
-            socketConnection("127.0.0.1", 50209);
+
     }
 	
     ext.digital_pin_mode = function (able, pin, mode) {
@@ -57,7 +48,7 @@
         send("/digital_pin_mode/" + able + "/" + pin + "/" + mode);
 	}
 	
-    ext.analog_pin_mode = function (able, pin){
+    ext.analog_pin_mode = function (able, pin, mode){
         if(able == "啟用") able = "%E5%95%9F%E7%94%A8";
         else if(able == "停用") able = "%E5%81%9C%E7%94%A8";
         
@@ -65,7 +56,7 @@
         else if(mode == "輸入(pull-up)") mode = "pull-up";
         else if(mode == "輸入(pull-down)") mode = "pull-down";
 		
-        send("/analog_pin_mode/" + able + "/" + pin);
+        send("/analog_pin_mode/" + able + "/" + pin + "/" + mode);
 		
 	}
 	
@@ -100,6 +91,10 @@
         var value = sensor_data[pin];
         return value;
 	}
+    
+    ext.shiftout = function(pin, pin, type, value){
+        send("/shiftout/" + pin + "/" + pin + "/" + type + "/" + value);
+    }
 	
     ext.s0 = function(){
         return sensor_data["s0"];
@@ -110,27 +105,78 @@
 	}
 	
     ext.sensor_update_scratch = function(ip, key, value){
-        send("/sensor_update_scratch/" + 0 + "/" + ip + "/" + key + "/" + value);
+        send("/sensor_update_scratch/" + "/" + ip + "/" + key + "/" + value);
 	}
 	
     ext.sensor_update = function(key, value){
-        send("/sensor_update/" + 0 + "/" + key + "/" + value);
+        send("/sensor_update/" + "/" + key + "/" + value);
 	}
 	
     ext.HTTPvalue = function(){
-        return sensor_data["HTTPvalue"];
+        if(sensor_data["HTTPvalue"] == "true")
+            return true;
+        else
+            return false;
+	}
+    
+    ext.HTTPvalue_number = function(){
+        return sensor_data["HTTPvalue_number"];
+	}
+    
+    ext.HTTPvalue_processed = function(){
+        return sensor_data["HTTPvalue_processed"];
+	}
+    
+    ext.HTTPvalue_last = function(){
+        return sensor_data["HTTPvalue_last"];
+	}
+    
+    ext.HTTP_allkeyValue = function(){
+        return sensor_data["HTTP_allkeyValue"];
+	}
+    
+    ext.HTTP_keyValue = function(){
+        return sensor_data["HTTP_keyValue"];
 	}
 	
     ext.httpPOST = function(url){
         url = replaceAll(url,"/","%2F")
+        url = replaceAll(url,"&","%26")
+        url = replaceAll(url,"?","%3F")
         send("/httpPOST/" + url);
 	}
 	
-    ext.httpGET = function(url){
+    ext.httpGET_type = function(type, url){
         url = replaceAll(url,"/","%2F")
-        send("/httpGET/" + url);
+        url = replaceAll(url,"&","%26")
+        url = replaceAll(url,"?","%3F")
+        send("/httpGET_type/" + type + "/" + url);
+	}
+    
+    ext.httpGET_database = function(type, value){
+        send("/httpGET_database/" + type + "/" + value);
+	}
+    
+    ext.jsonDataSelect_thingspeak = function(type, value){
+        send("/jsonDataSelect_thingspeak/" + type + "/" + value);
+	}
+    
+    ext.jsonDataSelect = function(type, value){
+        send("/jsonDataSelect/" + type + "/" + value);
+	}
+    
+    ext.jsonDataSelect_section = function(type, value){
+        send("/jsonDataSelect_section/" + type + "/" + value);
 	}
 	
+    ext.keyFind = function(){
+        send("/keyFind/");
+	}
+    
+    ext.keySelect = function(url){
+        send("/keySelect/" + url);
+	}
+    
     ext.voicedata = function(){
         return decodeURI(sensor_data["voicedata"]);
 	}
@@ -154,29 +200,89 @@
     ext.voiceVolume = function(value){
         send("/voiceVolume/" + value);
 	}
-		
-    function send(cmd) {
-        connection.send(cmd);
+    
+    ext.datavalue = function(){
+        return sensor_data["datavalue"];
     }
     
-    function send_poll(){
-        $.ajax({
-      type: "GET",
-      dataType: "text",
-      url: "http://127.0.0.1:50209/",
-      success: function(data) {
-          isConnected = true;
-        //console.log(data);
-      },
-      error: function(jqxhr, textStatus, error) {
-        console.log("Error downloading ISS data");
-      }
-    });
-        }
-        
-    /*function send_poll() {
+    ext.datavalue_line = function(){
+        return sensor_data["datavalue_line"];
+    }
+    
+    ext.opendata = function(value){
+        send("/opendata/" + value);
+    }
+    
+    ext.opendata_line = function(value, num){
+        send("/opendata_line/" + value + "/" + num);
+    }
+    
+    ext.writedata = function(input, value){
+        send("/writedata/" + input + "/" + value);
+    }
+    
+    ext.appenddata = function(input, value){
+        send("/appenddata/" + input + "/" + value);
+    }
+    
+    ext.open_notepad = function(value){
+        send("/open_notepad/" + value);
+    }
+    
+    ext.openBrowser = function(url){
+        url = replaceAll(url,"/","%2F")
+        url = replaceAll(url,"&","%26")
+        url = replaceAll(url,"?","%3F")
+        send("/openBrowser/" + url);
+    }
+    
+    ext.humidity_dht11 = function(){
+        return sensor_data["humidity_dht11"];
+    }
+    
+    ext.temperature_dht11 = function(){
+        return sensor_data["temperature_dht11"];
+    }
+    
+    ext.dht11 = function(value){
+        send("/dht11/" + value);
+    }
+    
+    ext.distance_HC_SR04 = function(){
+        return sensor_data["distance_HC-SR04"];
+    }
+    
+    ext.distance = function(pin, pin2){
+        send("/distance/" + pin + "/" + pin2);
+    }
+    
+    ext.IR_data = function(){
+        return sensor_data["IR_data"];
+    }
+    
+    ext.IR_send = function(type, value){
+        send("/IR_send/" + type + "/" + value);
+    }
+    
+    ext.IR_receive = function(type, value){
+        send("/IR_receive/" + type + "/" + value);
+    }
+		
+    function send(cmd) {
+        //connection.send(cmd);
         var http = new XMLHttpRequest();
-        http.open("GET", "http://127.0.0.1:50209/poll", true);
+        http.open("POST", "http://127.0.0.1:50213" + cmd, true);
+        http.onreadystatechange = function() {
+            if (http.readyState == 4) {
+                console.log(http.responseText);
+            }
+        }
+        http.send();
+    }
+	
+    function send_poll() {
+        var http = new XMLHttpRequest();
+        http.open("GET", "http://127.0.0.1:50213/poll", true);
         http.onreadystatechange = function() {
             if (http.readyState == 4) {
                 if (http.responseText.length != 0){
@@ -188,73 +294,79 @@
             }
         }
         http.send();
-    }*/
-    
-     $(window).load(function() {
-            console.log("ok");
-        });
-	
-    function socketConnection(ip, port) {
-        connection = new WebSocket('ws://' + ip + ':' + port);
-        connection.onopen = function (e) {
-            isConnected = true;
-        };
-        connection.onclose = function (e) {
-            isConnected = false;
-        };
-        connection.onmessage = function (e) {
-            //console.log(e.data);
-            var sensor = e.data.split("\n");
-			for(var i = 0;i < sensor.length;i++) sensor_data[sensor[i].split(" ")[0].toString()] = sensor[i].split(" ")[1];
-        };
-        connection.onerror = function (e) {
-            isConnected = false;
-        };
     }
 	
     function replaceAll(str, find, replace) {
-        while(str.search(find) != -1) str = str.replace(find, replace);
+        while(str.indexOf(find) >= 0) { str = str.replace(find, replace); }
     return str;
 }
 
     var descriptor = {
         blocks: [
-            [' ', '連接 Transformer', 'connect'],
-            [" ", "%m.pin_state : 數位腳位 %n 為 %m.digital_pin_mode", "digital_pin_mode", "啟用", "號碼", "輸入"],
-            [" ", "%m.pin_state : 類比腳位(A) %n 為 %m.analog_pin_mode", "analog_pin_mode", "啟用", "號碼", "輸入"],
-            ["", "數位輸出: 設定腳位 %n 為 %m.high_low", "digital_write", "號碼", 0],
-            ["", "模擬類比輸出(PWM): 設定腳位 %n 的值為  %n", "analog_write", "號碼", "數量值"],
-            ["", "在腳位 %n 播放音調, 頻率為: %n Hz, 時間為: %n ms", "play_tone", "號碼", 1000, 500],
-            ["", "關閉腳位 %n 的音調", "tone_off", "號碼"],
-            ["", "設定第 %n 腳位為伺服機輸出 轉動角度為 %n", "set_servo_position", "號碼", 90],
-            ["r", "讀取數位腳位 %n 的值", "digital_read", "號碼"],
-            ["r", "讀取類比腳位(A) %n 的值", "analog_read", "號碼"],
+            [" ", "%m.pin_state : 數位腳位 %d.digital_pin 為 %m.digital_pin_mode", "digital_pin_mode", "啟用", "號碼", "輸入"],
+            [" ", "%m.pin_state : 類比腳位(A) %d.analog_pin 為 %m.analog_pin_mode", "analog_pin_mode", "啟用", "號碼", "輸入"],
+            ["", "數位輸出: 設定腳位 %d.digital_pin 為 %d.high_low", "digital_write", "號碼", 0],
+            ["", "模擬類比輸出(PWM): 設定腳位 %d.pwm_pin 的值為  %n", "analog_write", "號碼", "數量值"],
+            ["", "在腳位 %d.digital_pin 播放音調, 頻率為: %n Hz, 時間為: %n ms", "play_tone", "號碼", 1000, 500],
+            ["", "關閉腳位 %d.digital_pin 的音調", "tone_off", "號碼"],
+            ["", "設定第 %d.digital_pin 腳位為伺服機輸出 轉動角度為 %n", "set_servo_position", "號碼", 90],
+            ["r", "讀取數位腳位 %d.digital_pin 的值", "digital_read", "號碼"],
+            ["r", "讀取類比腳位(A) %d.analog_pin 的值", "analog_read", "號碼"],
+            ["", "shiftout Din接(D) %n ,Clk接(D) %n ,資料格式: %m.shiftout_type ,資料: %n", "shiftout", "號碼", "號碼", "MSBFIRST", 0],
+            ["r", "資料", "datavalue"],
+            ["r", "資料行數", "datavalue_line"],
+            [" ", "讀取本機資料 從 %s", "opendata", "temp.txt"],
+            [" ", "讀取本機資料 從 %s 第 %n 行", "opendata_line", "temp.txt", 1],
+            [" ", "儲存資料 %s 在 %s", "writedata", "", "temp.txt"],
+            [" ", "附加資料 %s 在 %s", "appenddata", "", "temp.txt"],
+            [" ", "開啟檔案 %s", "open_notepad", "temp.txt"],
             ["r", "virtual sensor s0",  "s0"],
             ["r", "virtual sensor s1",  "s1"],
-            ["w", "向ip: %s 傳送變數 %s 值 %s", "sensor_update_scratch", "127.0.0.1:50209", "s0", 0],
-            ["w", "send %s value %n", "sensor_update", "temp", 255],
-            ["r", "HTTP GET 資料", "HTTPvalue"],
-            [" ", "HTTP POST 資料 %s", "httpPOST", ""],
-            [" ", "HTTP GET 資料 從 %s", "httpGET", ""],
-            ["r", "語音資料", "voicedata"],
-            [" ", "錄音 %n 秒", "record", 3],
-            [" ", "%s 轉語音(中文)", "textovoice_tw", "文字"],
-            [" ", "%s 轉語音(英文)", "textovoice_en", "word"],
-            [" ", "語音速度 %m.speed", "voiceSpeed", 0],
-            [" ", "語音音量 %m.volume", "voiceVolume", 100]
+            [" ", "send %s value %n", "sensor_update", "temp", 255],
+            [" ", "向ip: %s 傳送變數 %s 值 %s", "sensor_update_scratch", "127.0.0.1:50209", "s0", 0],
+            [" ", "開啟網頁 %s", "openBrowser", "http://www.kodorobot.com"],
+            ["b", "資料庫連接", "HTTPvalue"],
+            ["r", "雲端資料", "HTTPvalue_processed"],
+            ["r", "雲端資料筆數", "HTTPvalue_number"],
+            ["r", "最後一筆", "HTTPvalue_last"],
+            ["r", "可使用的欄位", "HTTP_allkeyValue"],
+            ["r", "選擇的欄位", "HTTP_keyValue"],
+            [" ", "HTTP POST 資料 %s", "httpPOST", "https://api.thingspeak.com/update?api_key=<api_key>&field1=<value>"],
+            [" ", "HTTP GET 資料 類型:%m.type 從 %s", "httpGET_type", "json_thingspeak", "https://thingspeak.com/channels/<channel_ID>/feed.json?results=8000"],
+            [" ", "HTTP GET 資料庫:%m.database channel ID: %s", "httpGET_database", "thingspeak", ""],
+            [" ", "雲端資料選擇 欄位:%s 第 %n 筆的剖析資料", "jsonDataSelect", "field1", "1"],
+            [" ", "選擇 第 %n 筆到第 %n 筆的剖析資料", "jsonDataSelect_section", "1", "10"],
+            [" ", "剖析可使用欄位", "keyFind"],
+            [" ", "選擇第 %n 筆欄位", "keySelect", "1"],
+            ["r", "DHT11 濕度(%)", "humidity_dht11"],
+            ["r", "DHT11 溫度(°C)", "temperature_dht11_°C"],
+            ["r", "DHT11 溫度 類型: %m.type3", "temperature_dht11", "°C"],
+            [" ", "DHT11 溫濕度感測器(D) %n", "dht11", 2],
+            ["r", "HC-SR04 距離", "distance_HC_SR04"],
+            [" ", "距離感測器HC-SR04 trig %n echo(A) %n", "distance", 10, 0],
+            ["r", "紅外線資料", "IR_data"],
+            [" ", "發射訊號 種類:%m.type2 %s (uno:D3 mega:D11)", "IR_send", "30個以內", ""],
+            [" ", "紅外線接收器 種類:%m.type2 秒數:%n (D2)", "IR_receive", "30個以內", 10],
 		],
         menus: {
             pin_state: ['啟用', '停用'],
             digital_pin_mode: ['輸入',"輸入(pull-up)","輸入(pull-down)", '輸出', 'PWM', '伺服機', '音調'],
             analog_pin_mode: ["輸入", "輸入(pull-up)", "輸入(pull-down)"],
             high_low: ["0", "1"],
-            speed : [-5,-4,-3,-2,-1,0,1,2,3,4,5],
-            volume : [0,10,20,30,40,50,60,70,80,90,100]
+            digital_pin: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "A0", "A1", "A2", "A3", "A4", "A5"],
+            analog_pin: ["0", "1", "2", "3", "4", "5"],
+            pwm_pin: ["3", "5", "6", "9", "10", "11"],
+            type: ["raw", "json_thingspeak", "json_opendata", "json_google"],
+            key: ["field1", "field2"],
+            database: ["thingspeak"],
+            type2: ["30個以內", "冷氣"],
+            type3: ["°C", "°F"],
+            shiftout_type: ["MSBFIRST", "LSBFIRST"],
     },
         url: 'https://kodorobot.github.io/scratchx/'
   };
 
-    ScratchExtensions.register('S2Aplus', descriptor, ext);
+    ScratchExtensions.register('S2Aplus_net_library', descriptor, ext);
 
 
 })({});
